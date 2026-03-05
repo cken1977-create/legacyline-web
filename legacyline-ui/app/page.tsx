1e87a6062319262b7db281c316531233989a3323
cken1977-create <cken1977@gmail.com>
2026-03-05 01:41:16 +0000

    Initial Legacyline UI

---
+++ legacyline-ui/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+        return (
+            <main style={{
+                  background:"#0b0f1a",
+                        color:"white",
+                              minHeight:"100vh",
+                                    display:"flex",
+                                          justifyContent:"center",
+                                                alignItems:"center",
+                                                      flexDirection:"column",
+                                                            fontFamily:"sans-serif"
+                                                                }}>
+                                                                      <h1 style={{fontSize:"42px"}}>Legacyline</h1>
+                                                                            <p>Behavioral Readiness Engine</p>
+                                                                                </main>
+                                                                                  );
+                                                                                  }
+}
