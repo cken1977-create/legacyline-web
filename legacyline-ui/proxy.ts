bc5ad0211cb3f4ed4b499a4b91b789468fa42d9e
cken1977-create <cken1977@gmail.com>
2026-03-15 11:21:18 -0500

    Update proxy.ts

--- legacyline-ui/proxy.ts
+++ legacyline-ui/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PROTECTED = ["/dashboard", "/intake"];
+const PROTECTED = ["/dashboard", ];
 
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
